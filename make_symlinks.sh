951a7b4f8a45bed1a595697b4c79c059597ae1b8
Kirt Lillywhite <kirt@smartvisionworks.com>
2013-09-11 11:54:51 -0600

    Delete targets before making symlinks.

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,5 +1,24 @@
+if [ -e ~/.vim ]; then
+	/bin/rm -rf ~/.vim
+fi
 ln -s ~/dotfiles/vim ~/.vim
+
+if [ -e ~/.vimrc ]; then
+	/bin/rm ~/.vimrc
+fi
 ln -s ~/dotfiles/vimrc ~/.vimrc
+
+if [ -e ~/.gvimrc ]; then
+	/bin/rm ~/.gvimrc
+fi
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
+
+if [ -e ~/.bashrc ]; then
+	/bin/rm ~/.bashrc
+fi
 ln -s ~/dotfiles/bashrc ~/.bashrc
+
+if [ -e ~/.zshrc ]; then
+	/bin/rm ~/.zshrc
+fi
 ln -s ~/dotfiles/zshrc ~/.zshrc
