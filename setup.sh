f30a1a9fcb61d7960e219d29b8eb5a8b4de845ae
Kirt Lillywhite <kfrance@wayne.local.svw>
2017-02-14 16:24:37 -0700

    Changed symbolic links to be relative rather than absolute.
    * This allows me to rsync my home director to another user (local svw
    user) and have everything work.

--- setup.sh
+++ setup.sh
@@ -2,39 +2,38 @@
 if [ -e ~/.vim ]; then
 	/bin/rm -rf ~/.vim
 fi
-ln -s ~/dotfiles/vim ~/.vim
-
 if [ -e ~/.vimrc ]; then
 	/bin/rm ~/.vimrc
 fi
-ln -s ~/dotfiles/vimrc ~/.vimrc
-
 if [ -e ~/.gvimrc ]; then
 	/bin/rm ~/.gvimrc
 fi
-ln -s ~/dotfiles/gvimrc ~/.gvimrc
-
 if [ -e ~/.bashrc ]; then
 	/bin/rm ~/.bashrc
 fi
-ln -s ~/dotfiles/bashrc ~/.bashrc
-
 if [ -e ~/.zshrc ]; then
 	/bin/rm ~/.zshrc
 fi
-ln -s ~/dotfiles/zshrc ~/.zshrc
-
 if [ -e ~/.ssh/config ]; then
 	/bin/rm ~/.ssh/config
 fi
-ln -s ~/dotfiles/ssh_config ~/.ssh/config
-
 if [ -e ~/.bash_profile ]; then
 	/bin/rm ~/.bash_profile
 fi
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
+cd ~
+
+ln -s dotfiles/vim .vim
+
+ln -s dotfiles/vimrc .vimrc
+
+ln -s dotfiles/gvimrc .gvimrc
+
+ln -s dotfiles/bashrc .bashrc
+
+ln -s dotfiles/zshrc .zshrc
+
+ln -s dotfiles/ssh_config .ssh/config
+
+ln -s dotfiles/bash_profile .bash_profile
 
 chmod 644 ~/dotfiles/ssh_config
-
-# Install trash
-sudo pacman -Sy trash-cli
