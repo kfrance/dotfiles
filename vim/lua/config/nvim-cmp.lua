ed84e033502d098874eda39d61046a4b33e190ce
Kirt Lillywhite <kirt@smartvisionworks.com>
2022-11-02 16:30:41 -0600

    Got nvim-cmp to work more like super tab.

--- vim/lua/config/nvim-cmp.lua
+++ vim/lua/config/nvim-cmp.lua
@@ -2,6 +2,15 @@
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -9,26 +18,32 @@ cmp.setup {
       vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
-    ["<Tab>"] = function(fallback)
+  mapping = {
+
+    -- ... Your other mappings ...
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
-        fallback()
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      else
-        fallback()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end,
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<Esc>"] = cmp.mapping.close(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    end, { "i", "s" }),
+
+    -- ... Your other mappings ...
+
   },
   sources = {
     { name = "nvim_lsp" }, -- For nvim-lsp
@@ -39,8 +54,8 @@ cmp.setup {
     { name = "emoji", insert = true }, -- emoji completion
   },
   completion = {
-    keyword_length = 1,
-    completeopt = "menu,noselect",
+		  autocomplete = false,
+		  completeopt = "menu,noselect",
   },
   view = {
     entries = "custom",
